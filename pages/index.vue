bafd9f1ae8f109869c70c6dbe91c2307784cdfd2
fuxj5 <fuxj5@asiainfo.com>
2023-08-22 11:39:41 +0800

    feat:init nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <div class="bg-pink">
+    <div>123123123</div>
+  </div>
+</template>
+
+<style scoped></style>
